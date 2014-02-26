65a2f5f0e2a833730c8495551b962b01980539c5
Lauren Voswinkel <lauren.voswinkel@livingsocial.com>
2014-02-25 21:38:21 -0500

    Check identities and card numbers

--- client/deck_list.js
+++ client/deck_list.js
@@ -10,11 +10,13 @@ function DeckList(deck_list){
       }
       var card = card_id // TODO: Replace with card instantiation
         , times_in_deck = deck_json[card_id];
+      if(times_in_deck > 3){ throw Error("Deck list cannot have more than 3 of any given card"); }
       for(i = 0; i < times_in_deck; i++) deck_list.cards.push(card);
     }
   }
 
   function assignIdentity(deck_list, id){
+    if(deck_list.identity != undefined){ throw Error("Deck list cannot have more than one identity") };
     deck_list.identity = id;
   }
 
--- test/client/deck_list_test.js
+++ test/client/deck_list_test.js
@@ -15,14 +15,33 @@ describe('DeckList', function(){
   });
 
   describe('#new', function(){
-    it('creates a DeckList from a JSON object', function(){
-      dl = new DeckList({"00001":3, "00002":2, "02031":1});
-      expect(dl).to.be.a(DeckList);
-      expect(dl.cards.length).to.be(5);
+    var legit_deck_list;
+    beforeEach(function(){
+      legit_deck_list = {"00001":3,"00002":3,"00003":3,"00004":3,"00005":3
+                        ,"00011":3,"00012":3,"00013":3,"00014":3,"00015":3
+                        ,"00021":3,"00022":3,"00023":3,"00024":3,"00025":3
+                        ,"02031":1};
     });
 
-    it('throws an error if no identity is given', function(){
-      expect(DeckList).withArgs({"00002":3, "00004":2}).to.throwException(/deck list has no identity/i);
+    it('creates a DeckList from a JSON object', function(){
+      dl = new DeckList(legit_deck_list);
+      expect(dl).to.be.a(DeckList);
+      expect(dl.cards.length).to.be(45);
+    });
+
+    it('requires an identity', function(){
+      delete legit_deck_list["02031"];
+      expect(DeckList).withArgs(legit_deck_list).to.throwException(/deck list has no identity/i);
+    });
+
+    it('verifies that there are no more than three copies per card', function(){
+      legit_deck_list["00001"] = 4;
+      expect(DeckList).withArgs(legit_deck_list).to.throwException(/deck list cannot have more than 3 of any given card/i);
+    });
+
+    it('verifies that there are not two identities', function(){
+      legit_deck_list["05030"] = 1;
+      expect(DeckList).withArgs(legit_deck_list).to.throwException(/deck list cannot have more than one identity/i);
     });
   });
 
